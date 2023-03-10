6c6481aa3d787071c6f37fcf6fd99ff2421d8b01
hahwul <hahwul@gmail.com>
2023-03-10 18:51:04 +0900

    Update parser

--- src/crystal-smuggle.cr
+++ src/crystal-smuggle.cr
@@ -9,6 +9,24 @@ end
 OptionParser.parse do |parser|
   parser.banner = "CRYSTAL-SMUGGLE"
 
+  parser.on "-u", "--url", "Target URL from argument" do
+
+  end
+  parser.on "-p", "--pipeline", "Target URL from stdin" do
+
+  end
+  parser.on "-X", "--method", "Set HTTP Request Method" do
+    
+  end
+  parser.on "-H", "--headers", "Set HTTP Request Headers" do
+    
+  end
+  parser.on "-t", "--timeout", "Set Timeout" do
+    
+  end
+  parser.on "-o", "--output", "Output file" do
+    
+  end
   parser.on "-v", "--version", "Show version" do
     puts Crystal::Smuggle::VERSION
     exit
